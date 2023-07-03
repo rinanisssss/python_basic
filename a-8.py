7dee4115bea8fabbf78dc6362d9484bfc7965b84
rinanisssss <rina.nishizaki0124mafu@gmail.com>
2023-07-03 11:36:21 +0900

    a-10 finish

--- a-8.py
+++ a-8.py
@@ -1,3 +1,8 @@
 users_info = [["Bob", 79], ["Tom", 59], ["Ken", 61]]
 
-print(users_info)
+# わからなかったので答えみました。
+for user in users_info:
+    name = user[0]
+    age = user[1]
+
+    print(f"Name: {name}, Age {age}")
